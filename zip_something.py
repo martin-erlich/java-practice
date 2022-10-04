f9dd9e69382b0369b6bb64f542fcc0fa05e36437
martin-erlich <64599962+martin-erlich@users.noreply.github.com>
2022-10-04 15:35:58 -0300

    Delete zip_something.py

--- zip_something.py
+++
@@ -1,4 +0,0 @@
-import os
-lesson = input('Lesson number: ')
-os.system(f'cd lesson_{lesson} && zip lesson_{lesson}.zip *.java')
-os.system(f'mv lesson_{lesson}/lesson_{lesson}.zip lesson_{lesson}.zip')
