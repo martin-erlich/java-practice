a15e2f0beb3173d92b27373e2f3244d7e51d20f4
martin-erlich <martoerlich@gmail.com>
2022-10-23 22:02:03 +0100

    Fixing checkerboard

--- lesson_4/Checkerboard.java
+++ lesson_4/Checkerboard.java
@@ -1,29 +1,31 @@
 public class Checkerboard {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        StdDraw.setScale(0, 1);
+        StdDraw.setScale(0, n);
         int i;
         int j;
-        double w = 1.0 / (n * 2.0);
-        double y = 1.0 / (n * 2.0);
+        double w = 0.5;
+        double y = 0.5;
         for (j = 1; j <= n; j++) {
-            double x = 1.0 / (n * 2.0);
+            double x = 0.5;
 
             for (i = 1; i <= n; i++) {
                 if (i % 2 > 0 && j % 2 > 0) {
+                    StdDraw.setPenColor(StdDraw.BLUE);
                     StdDraw.filledSquare(x, y, w);
                 } else if (i % 2 > 0 && j % 2 == 0) {
-                    StdDraw.square(x, y, w);
+                    StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+                    StdDraw.filledSquare(x, y, w);
                 } else if (i % 2 == 0 && j % 2 == 0) {
+                    StdDraw.setPenColor(StdDraw.BLUE);
                     StdDraw.filledSquare(x, y, w);
                 } else if (i % 2 == 0 && j % 2 > 0) {
-                    StdDraw.square(x, y, w);
+                    StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+                    StdDraw.filledSquare(x, y, w);
                 }
                 x += (w * 2.0);
             }
             y += (w * 2.0);
         }
-
     }
-
 }
